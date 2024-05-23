19d73b006eade8bff53c11289243c485941ae26a
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-05-22 21:58:30 -0400

    added chamges

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs")
 app.use(express.static("./styles"))
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{
+    res.send("home")
+});
+app.get("/about", (req, res) =>{
+    res.send("about page");
+});
+
+app.get("/user/:name", (req, res) =>{
+    const userName = req.params.name;
+    res.send("user", {name: userName});
+});
+
 
 app.listen(PORT, () =>{
     console.log("It is running")
